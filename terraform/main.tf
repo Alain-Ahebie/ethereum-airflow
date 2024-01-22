1e722942685b86cd6d434f055d63f5cf1bdf24b5
alainahebie <alainahebie@gmail.com>
2024-01-22 20:18:11 +0100

    add ssh key

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,59 +52,73 @@ resource "google_bigquery_table" "evm_transactions" {
 # This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
 # For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
 
-# resource "google_compute_instance" "eth-collect" {
-#   boot_disk {
-#     auto_delete = true
-#     device_name = "eth-collect"
+resource "google_compute_instance" "eth-collect" {
+  boot_disk {
+    auto_delete = true
+    device_name = "eth-collect"
 
-#     initialize_params {
-#       image = "projects/debian-cloud/global/images/debian-11-bullseye-v20240110"
-#       size  = 10
-#       type  = "pd-balanced"
-#     }
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20240110"
+      size  = 10
+      type  = "pd-balanced"
+    }
 
-#     mode = "READ_WRITE"
-#   }
+    mode = "READ_WRITE"
+  }
 
-#   can_ip_forward      = false
-#   deletion_protection = false
-#   enable_display      = true
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = true
 
-#   labels = {
-#     goog-ec-src = "vm_add-tf"
-#   }
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
 
-#   machine_type = "e2-medium"
-#   name         = "eth-collect"
+  machine_type = "e2-medium"
+  name         = "eth-collect"
 
-#   network_interface {
-#     access_config {
-#       network_tier = "PREMIUM"
-#     }
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
 
-#     queue_count = 0
-#     stack_type  = "IPV4_ONLY"
-#     subnetwork  = "projects/plomber/regions/us-central1/subnetworks/default"
-#   }
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/plomber/regions/us-central1/subnetworks/default"
+  }
 
-#   scheduling {
-#     automatic_restart   = true
-#     on_host_maintenance = "MIGRATE"
-#     preemptible         = false
-#     provisioning_model  = "STANDARD"
-#   }
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
 
-#   service_account {
-#     email  = "908912453933-compute@developer.gserviceaccount.com"
-#     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-#   }
+  service_account {
+    email  = "908912453933-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 
-#   shielded_instance_config {
-#     enable_integrity_monitoring = true
-#     enable_secure_boot          = false
-#     enable_vtpm                 = true
-#   }
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
 
-#   tags = ["http-server", "https-server", "lb-health-check"]
-#   zone = "us-central1-c"
-# }
+  tags = ["http-server", "https-server", "lb-health-check"]
+  zone = "us-central1-c"
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/alaingcp2023/python"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "alaingcp2023"  # The username associated with your SSH key
+    private_key = file("/home/alaingcp2023/.ssh/my-ssh-key")  # Path to your generated private key
+    host        = self.network_interface[0].access_config[0].nat_ip
+  }
+
+}
