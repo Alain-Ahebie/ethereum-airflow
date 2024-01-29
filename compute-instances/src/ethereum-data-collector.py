f26890bcce0415eab0c096139a66875380da051e
alainahebie <alainahebie@gmail.com>
2024-01-29 09:14:56 +0100

    push parquet to gcs bucket

--- compute-instances/src/ethereum-data-collector.py
+++ compute-instances/src/ethereum-data-collector.py
@@ -2,6 +2,7 @@ import datetime
 import pandas as pd
 from web3 import Web3
 from web3.exceptions import Web3Exception
+from google.cloud import storage
 
 def connect_to_ethereum_node(url):
     # Connects to an Ethereum node using the provided URL
@@ -17,7 +18,7 @@ def connect_to_ethereum_node(url):
     
 def get_block_one_hour_ago(web3, latest_block):
     # Finds the block number that was closest to one hour ago
-    one_hour_ago = datetime.datetime.now() - datetime.timedelta(minutes=2)
+    one_hour_ago = datetime.datetime.now() - datetime.timedelta(minutes=1)
     block_number = latest_block
     while True:
         try:
@@ -56,16 +57,16 @@ def get_transactions(web3, start_block, end_block):
                     "BlockTimestamp": datetime.datetime.fromtimestamp(block.timestamp),  # Time when the block was mined
                     "TransactionIndex": tx.transactionIndex,  # Transaction's index position in the block
                     "CumulativeGasUsed": receipt.cumulativeGasUsed,  # Cumulative gas used in the block up to this transaction
-                    "Logs": [log for log in receipt.logs],  # Event logs emitted by this transaction
-                    "LogsBloom": receipt.logsBloom.hex(),  # Bloom filter for light clients
+                    # "Logs": [dict(log) for log in receipt.logs]  ,  # Event logs emitted by this transaction
+                    # "LogsBloom": receipt.logsBloom.hex(),  # Bloom filter for light clients
                     "Status": receipt.status,  # Status of the transaction (1 = success, 0 = failure)
                     "ContractAddress": receipt.contractAddress if receipt.contractAddress else None,  # Contract address created, if any
                     "Root": receipt.root if 'root' in receipt else None,  # State root after the transaction (pre-Byzantium forks)
                     "IsError": 0 if receipt.status == 1 else 1  # Flag indicating if the transaction was erroneous
                 })
-                print(transactions)
         except Web3Exception as e:
             print(f"An error occurred while processing block {block_number}: {str(e)}")
+            pass  # Continue with the next iteration even if an error occurs
     return transactions
 
 def save_to_parquet(data, filename):
@@ -73,9 +74,27 @@ def save_to_parquet(data, filename):
     df = pd.DataFrame(data)
     df.to_parquet(filename)
     
-INFURA_URL = "https://mainnet.infura.io/v3/12765045634040aba2c2ae29a97be8d4"   
-web3 = connect_to_ethereum_node(INFURA_URL)
-latest_block = web3.eth.get_block('latest').number
-start_block = get_block_one_hour_ago(web3, latest_block)    
-transactions = get_transactions(web3, start_block, latest_block)
-save_to_parquet(transactions, 'ethereum_transactions_last_hour.parquet')
+    
+def upload_to_gcs(bucket_name, source_file_name):
+    """Uploads a file to Google Cloud Storage, organized by year/month/day."""
+    # Determine the current date and time for folder organization
+    now = datetime.datetime.now()
+    destination_blob_name = f"year={now.year}/{now.strftime('%m')}/{now.strftime('%d')}/{source_file_name}"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+    print(f"File {source_file_name} uploaded to {destination_blob_name} in bucket {bucket_name}.")    
+    
+# INFURA_URL = "https://mainnet.infura.io/v3/12765045634040aba2c2ae29a97be8d4"   
+# web3 = connect_to_ethereum_node(INFURA_URL)
+# latest_block = web3.eth.get_block('latest').number
+# start_block = get_block_one_hour_ago(web3, latest_block)    
+# transactions = get_transactions(web3, start_block, latest_block)
+# save_to_parquet(transactions, 'ethereum_transactions_last_min.parquet')
+
+
+upload_to_gcs('evm_bucket', "ethereum_transactions_last_min.parquet")
+
